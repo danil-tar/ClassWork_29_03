66cb7e92bc648c92f4c42fc8066ff9b8513a3a53
Dan <danilviktar@gmail.com>
2024-03-30 15:24:30 +0200

    snackbar testing

--- app/src/main/java/com/example/toast/MainActivity.java
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +16,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        @SuppressLint("ShowToast") Snackbar snackbar
+                = Snackbar.make(findViewById(R.id.button), "Hello World", Snackbar.LENGTH_SHORT);
+        snackbar.setAction("Ok", new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Snackbar Ok Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_SHORT).show();
+                snackbar.show();
+
             }
         });
-        
 
     }
 }
